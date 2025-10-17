fe4a3ef2d684df7622fa4135ec0358877078da21
Dhinara Shaffina <shaffinadhinara@gmail.com>
2025-10-17 11:30:06 +0700

    Update tokoSerbaAda.java

--- pertemuan7/tokoSerbaAda.java
+++ pertemuan7/tokoSerbaAda.java
@@ -2,58 +2,68 @@ package pertemuan7;
 
 import java.util.Scanner;
 
-public class tokoSerbaAda{
+public class tokoSerbaAda {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        String kodeBarang [] = {"BT001", "BT002", "BT003", "BT004"};
-        String namaBarang [] = {"Buku", "Pensil", "Pulpen", "Baju"};
-        int hargaBarang[] = {5000, 3000, 4000, 50000 };
-        int i;
+        Scanner scanner = new Scanner(System.in);
 
+        String kodeBarang[] = {"BT001", "BT002", "BT003", "BT004"};
+        String namaBarang[] = {"Buku", "Pensil", "Pulpen", "Baju"};
+        int hargaBarang[] = {5000, 3000, 4000, 50000};
+
+        System.out.println("\n********************************");
         System.out.println("Selamat Datang di Toko Serba Ada");
         System.out.println("********************************");
         System.out.print("Masukkan jumlah item barang : ");
         int jumlahItem = scanner.nextInt();
 
         String[] kode = new String[jumlahItem];
-        int[] jumlah= new int[jumlahItem];
+        int[] jumlah = new int[jumlahItem];
         String[] nama = new String[jumlahItem];
         int[] harga = new int[jumlahItem];
         int[] total = new int[jumlahItem];
 
-        for(i=0; i < jumlahItem; i++){
-            System.out.println("Data ke " + (i+1));
+        for (int i = 0; i < jumlahItem; i++) {
+            System.out.println("Data ke " + (i + 1));
             System.out.print("Masukkan kode barang : ");
             kode[i] = scanner.next();
             System.out.print("Masukkan jumlah beli : ");
             jumlah[i] = scanner.nextInt();
-        }
 
+
+            boolean find = false;
             for (int j = 0; j < kodeBarang.length; j++) {
-                if (kodeBarang[i].equalsIgnoreCase(kodeBarang[j])) {
-                        nama[i] = namaBarang[j];
-                        harga[i] = hargaBarang[j];
-                        total[i] = harga[i] * jumlah[i];
+                if (kode[i].equalsIgnoreCase(kodeBarang[j])) {
+                    nama[i] = namaBarang[j];
+                    harga[i] = hargaBarang[j];
+                    total[i] = harga[i] * jumlah[i];
+                    find = true;
+                    break; 
                 }
             }
 
-        System.out.println("\n TOKO SERBA ADA");
-        System.out.println("**************");
-        System.out.println("No\t kode Barang\t Nama Barang\t Harga\t Jumlah Beli\t Jumlah Bayar\t");
-        System.out.println("===============================================================");
-
-
-        for(i = 0; i< jumlahItem; i++){
-            System.out.println((i+1) + "\t" + kode[i] + "\t\t" + nama[i] + "\t\t" + harga[i] + "\t" + jumlah[i] + "\t\t" + total[i]);
+            if (!find) {
+                System.out.println("Kode barang " + kode[i] + " tidak ditemukan!");
+                nama[i] = "-";
+                harga[i] = 0;
+                total[i] = 0;
+            }
         }
 
+        System.out.println("\n\n**************");
+        System.out.println("TOKO SERBA ADA");
+        System.out.println("**************");
+        System.out.println("No\tKode Barang Nama Barang\tHarga Jumlah Beli Jumlah Bayar");
+        System.out.println("==============================================================");
+
         int totalBayar = 0;
-        System.out.println("===============================================================");
-        totalBayar += total[i];
-        System.out.println("Total Bayar\t\t\t\t\t" + totalBayar);
+        for (int i = 0; i < jumlahItem; i++) {
+            System.out.println((i + 1) + "\t\t" + kode[i] + "\t\t" + nama[i] + "\t" + harga[i] + "\t\t" + jumlah[i] + "\t\t\t" + total[i]);
+            totalBayar += total[i];
+        }
 
-
+        System.out.println("==============================================================");
+        System.out.println("Total Bayar : \t\t\t\t\t\t\t\t\t\t" + totalBayar);
+        System.out.println("==============================================================");
     }
-
-
 }
+
